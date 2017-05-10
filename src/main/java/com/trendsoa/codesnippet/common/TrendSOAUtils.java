696e34bb6886376c8c1a2327b86d9aa2981d44e5
jeszhang <jeszhang@trendsoa.com>
2017-05-11 00:08:02 +0800

    add tips

---
+++ src/main/java/com/trendsoa/codesnippet/common/TrendSOAUtils.java
@@ -0,0 +1,13 @@
+package com.trendsoa.codesnippet.common;
+
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.WebUtils;
+
+/**
+ * Created by jeszhang on 2017/5/10.
+ */
+public class TrendSOAUtils {
+
+
+
+}
--- src/main/java/com/trendsoa/codesnippet/controller/UserController.java
+++ src/main/java/com/trendsoa/codesnippet/controller/UserController.java
@@ -7,8 +7,13 @@ import com.trendsoa.codesnippet.model.UserRegistionRequest;
 import com.trendsoa.codesnippet.service.UserService;
 import com.trendsoa.codesnippet.validator.group.GroupOne;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by jeszhang on 08-05-2017.
@@ -38,6 +43,16 @@ public class UserController {
         userService.getEchoString(userLoginRequest.getEmail());
 
         return new User("Depeux", 20);
+    }
+
+    @RequestMapping(value = "/getHeader", method = RequestMethod.GET)
+    public String getHeader() {
+
+//        HttpComponentsClientHttpRequestFactory
+        //how to get request
+        HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+
+        return request.getHeader("test");
     }
 
 }
